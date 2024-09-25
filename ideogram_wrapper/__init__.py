72782965a34edfb57a0fac02bd6eea296529fbd7
M <132838549+MrGrasss@users.noreply.github.com>
2024-09-25 16:02:00 +0200

    Update __init__.py

--- ideogram_wrapper/__init__.py
+++ ideogram_wrapper/__init__.py
@@ -1,32 +1,27 @@
-"""
-Ideogram Wrapper
-Author: Flowese
-Version: 0.0.2
-Date: 2023-02-30
-Description: Generates images from Ideogram API using textual prompts.
-"""
+from datetime import datetime, timedelta
+from time import sleep
 
+import requests
+import logging
+import shutil
+import base64
 import os
 import re
-import requests
-import shutil
-import logging
-from time import sleep
-from datetime import datetime, timedelta
+
 
 class IdeogramWrapper:
     BASE_URL = "https://ideogram.ai/api/images"
-    
+
     def __init__(
-        self,
-        session_cookie_token,
-        prompt, 
-        user_id="-xnquyqCVSFOYTomOeUchbw",  # Argumento con valor predeterminado
-        channel_id="LbF4xfurTryl5MUEZ73bDw",
-        aspect_ratio="square",
-        output_dir="images", 
-        enable_logging=False):
-    
+            self,
+            session_cookie_token,
+            prompt,
+            user_id="-xnquyqCVSFOYTomOeUchbw",
+            channel_id="LbF4xfurTryl5MUEZ73bDw",
+            output_dir="images",
+            enable_logging=False,
+            in_memory=True):
+
         if not session_cookie_token:
             raise ValueError("Session cookie token is not defined.")
         if not prompt:
@@ -36,19 +31,20 @@ class IdeogramWrapper:
         self.channel_id = channel_id
         self.session_cookie_token = session_cookie_token
         self.prompt = prompt
-        self.aspect_ratio = aspect_ratio
         self.output_dir = output_dir
         self.enable_logging = enable_logging
+        self.in_memory = in_memory
+        self.downloaded_images = []
 
         if self.enable_logging:
-            logging.basicConfig(format='[%(asctime)s] [%(levelname)s]: %(message)s', 
+            logging.basicConfig(format='[%(asctime)s] [%(levelname)s]: %(message)s',
                                 level=logging.INFO, datefmt='%H:%M:%S')
             logging.info("IdeogramWrapper initialized.")
 
     def fetch_generation_metadata(self, request_id):
         url = f"{self.BASE_URL}/retrieve_metadata_request_id/{request_id}"
         headers, cookies = self.get_request_params()
-        
+
         try:
             response = requests.get(url, headers=headers, cookies=cookies)
             response.raise_for_status()
@@ -65,20 +61,22 @@ class IdeogramWrapper:
     def inference(self):
         url = f"{self.BASE_URL}/sample"
         headers, cookies = self.get_request_params()
-        
+
         payload = {
-            "aspect_ratio": self.aspect_ratio,
-            "channel_id": self.channel_id,
             "prompt": self.prompt,
-            "raw_or_fun": "raw",
-            "speed": "slow",
-            "style": "photo",
-            "user_id": self.user_id
+            "user_id": self.user_id,
+            "model_version": "V_1_5",
+            "use_autoprompt_option": "ON",
+            "sampling_speed": 0,
+            "style_expert": "AUTO",
+            "resolution": {
+                "width": 736,
+                "height": 1312
+            }
         }
-        
+
         try:
-            response = requests.post(url, headers=headers, 
-                                    cookies=cookies, json=payload)
+            response = requests.post(url, headers=headers, cookies=cookies, json=payload)
             response.raise_for_status()
 
             request_id = response.json().get("request_id")
@@ -90,7 +88,7 @@ class IdeogramWrapper:
 
     def make_get_request(self, request_id):
         end_time = datetime.now() + timedelta(minutes=5)
-        
+
         while datetime.now() < end_time:
             image_data = self.fetch_generation_metadata(request_id)
             if image_data:
@@ -100,32 +98,47 @@ class IdeogramWrapper:
 
     def download_images(self, responses):
         headers, cookies = self.get_request_params()
-        
+
         for i, response in enumerate(responses):
             image_url = f"{self.BASE_URL}/direct/{response['response_id']}"
-            file_path = self.download_image(image_url, headers, cookies, i)
-            
-        if file_path and self.enable_logging:
-            logging.info(f"Successfully downloaded {len(responses)} images to {self.output_dir}.")
+            if self.in_memory:
+                image_data = self.download_image_in_memory(image_url, headers, cookies)
+                if image_data:
+                    self.downloaded_images.append(image_data)
+            else:
+                file_path = self.download_image_to_disk(image_url, headers, cookies, i)
+                if file_path:
+                    self.downloaded_images.append(file_path)
 
-    def download_image(self, image_url, headers, cookies, index):
+        if self.downloaded_images and self.enable_logging:
+            logging.info(f"Successfully downloaded {len(self.downloaded_images)} images.")
+
+    def download_image_to_disk(self, image_url, headers, cookies, index):
         os.makedirs(self.output_dir, exist_ok=True)
 
         sanitized_prompt = re.sub(r'[^\w\s\'-]', '', self.prompt).replace(' ', '_')
         file_path = os.path.join(self.output_dir, f"{sanitized_prompt}_{index}.jpeg")
-        
+
         try:
-            response = requests.get(image_url, headers=headers, 
-                                    cookies=cookies, stream=True)
+            response = requests.get(image_url, headers=headers, cookies=cookies, stream=True)
             response.raise_for_status()
 
             with open(file_path, "wb") as f:
                 shutil.copyfileobj(response.raw, f)
             return file_path
         except requests.RequestException as e:
-            logging.error(f"An error occurred: {e}")
+            logging.error(f"An error occurred while downloading to disk: {e}")
         return None
-    
+
+    def download_image_in_memory(self, image_url, headers, cookies):
+        try:
+            response = requests.get(image_url, headers=headers, cookies=cookies, stream=True)
+            response.raise_for_status()
+            return base64.b64encode(response.content).decode('utf-8')
+        except requests.RequestException as e:
+            logging.error(f"An error occurred while downloading in memory: {e}")
+        return None
+
     def get_request_params(self):
         headers = {
             "Accept": "*/*",
